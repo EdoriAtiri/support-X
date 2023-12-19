d7f2aba7dc0305f35b37062a9cab4173b074c55f
Edori Atiri <59481349+EdoriAtiri@users.noreply.github.com>
2023-12-19 19:53:15 +0100

    refactor: add columns to supportRequest  model

--- app/Models/SupportRequest.ts
+++ app/Models/SupportRequest.ts
@@ -1,13 +1,31 @@
-import { DateTime } from 'luxon'
-import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import { DateTime } from "luxon";
+import { BaseModel, column } from "@ioc:Adonis/Lucid/Orm";
 
 export default class SupportRequest extends BaseModel {
   @column({ isPrimary: true })
-  public id: number
+  public id: number;
+
+  @column()
+  public first_name: string;
+
+  @column()
+  public last_name: string;
+
+  @column()
+  public email: string;
+
+  @column()
+  public title: string;
+
+  @column()
+  public message: string;
+
+  @column()
+  public file: string;
 
   @column.dateTime({ autoCreate: true })
-  public createdAt: DateTime
+  public createdAt: DateTime;
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
-  public updatedAt: DateTime
+  public updatedAt: DateTime;
 }
