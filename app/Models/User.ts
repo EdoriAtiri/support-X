6712a3605d6cc7daee59adcfd40c18f690f624b8
Edori Atiri <59481349+EdoriAtiri@users.noreply.github.com>
2023-12-19 19:43:36 +0100

    refactor: add columns to User model

--- app/Models/User.ts
+++ app/Models/User.ts
@@ -1,13 +1,22 @@
-import { DateTime } from 'luxon'
-import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import { DateTime } from "luxon";
+import { BaseModel, HasMany, column, hasMany } from "@ioc:Adonis/Lucid/Orm";
 
 export default class User extends BaseModel {
   @column({ isPrimary: true })
-  public id: number
+  public id: number;
+
+  @column()
+  public fullName: string;
+
+  @column()
+  public email: string;
+
+  @hasMany(() => Support_request)
+  public support_requests: HasMany<typeof Support_request>;
 
   @column.dateTime({ autoCreate: true })
-  public createdAt: DateTime
+  public createdAt: DateTime;
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
-  public updatedAt: DateTime
+  public updatedAt: DateTime;
 }
