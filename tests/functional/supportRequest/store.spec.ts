e358e6d5e5fd6f0ded6d9f605237676018bb2edf
Edori Atiri <59481349+EdoriAtiri@users.noreply.github.com>
2023-12-21 15:24:14 +0100

    refactor: rename testSupport to mockSupport in test

--- tests/functional/supportRequest/store.spec.ts
+++ tests/functional/supportRequest/store.spec.ts
@@ -1,35 +1,31 @@
 import { test } from "@japa/runner";
 
-import SupportRequest from "App/Models/SupportRequest";
-
-const testSupportRequest = {
+const mockSupportRequest = {
   first_name: "Saul",
   last_name: "Paul",
   email: "saul@email.com",
   title: "test",
   message: "tests support request handling",
 };
-let testSupportRequestId;
+let mockSupportRequestId;
 
 test.group("Support request store", () => {
   test("support request was submitted", async ({ client }) => {
-    const response = await client.post("/support").form(testSupportRequest);
-
-    // console.log(response.body().id);
+    const response = await client.post("/support").form(mockSupportRequest);
 
     // Assert that the submission was successful with status code of 200
     response.assertStatus(200);
-    testSupportRequestId = response.body().id;
+    mockSupportRequestId = response.body().id;
 
     // Verify that the data in response is the data in supportRequest submitted
-    response.assertBodyContains(testSupportRequest);
+    response.assertBodyContains(mockSupportRequest);
   });
 
   test("support request persists /support/request", async ({ client }) => {
     const response = await client
       .post("/support/request")
-      .form({ id: testSupportRequestId });
+      .form({ id: mockSupportRequestId });
 
-    response.assertBodyContains(testSupportRequest);
+    response.assertBodyContains(mockSupportRequest);
   });
 });
